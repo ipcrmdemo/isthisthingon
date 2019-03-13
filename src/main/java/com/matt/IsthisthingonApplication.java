f2fc94567cfdcb833bf6ce0a902446383c9e8494
Matt Cadorette <mcadorette@gmail.com>
2019-03-13 13:26:42 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/matt/IsthisthingonApplication.java
@@ -0,0 +1,12 @@
+package com.matt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsthisthingonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsthisthingonApplication.class, args);
+	}
+}
